1712dac323ffcfbb1e618f5350a4cb7481748f40
vijay517 <vijayganesh517@gmail.com>
2025-03-25 21:51:42 +0800

    implement translation feature

--- translate/views.py
+++ translate/views.py
@@ -4,7 +4,12 @@ from django.http import JsonResponse, HttpResponse, HttpRequest
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
 # Create your views here.
+load_dotenv()
 
 @method_decorator(csrf_exempt, name='dispatch')
 class Translate(View):
@@ -15,13 +20,41 @@ class Translate(View):
         if text is None:
             return HttpResponse("Missing text field")
         
-        print(text)
 
-        return JsonResponse({"message":"Translation complete"})
+        # roles: assistant, human, system
+        api_key = os.getenv("api_key")
+        print(api_key[:10])
+
+        client = OpenAI(api_key=api_key)
+
+        llm_response = client.chat.completions.create(
+            model='gpt-4o-mini',
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are translator that translates the given english text to mandarin. Do not entertain any other request"
+                },
+                {
+                    "role": "user",
+                    "content": text
+                },
+            ]
+        )
+        
+        return JsonResponse({"message":llm_response.choices[0].message.content})
         
 
     # try creating a get endpoint. 
 
 
 # Create an post endpoint for to-malay
-    
+
+
+# p3.10
+# p3.12
+
+# default : p3.10
+
+# create a venv using p3.12
+# activate the env
+# python3 -> p3.12
